e340e41bfdad308eb0b6bf48e8348cd7a339f7dc
Sarthak Panigrahi <sipunp70@gmail.com>
2024-04-06 23:23:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("bmiform").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const gender = document.getElementById("gender").value;
+  const age = parseInt(document.getElementById("age").value);
+  const heightFeet = parseInt(document.getElementById("height-feet").value);
+  const heightInches = parseInt(document.getElementById("height-inches").value);
+  const weight = parseFloat(document.getElementById("weight").value);
+  if (gender && age && heightFeet && heightInches && weight) {
+    const heightInMeters = (heightFeet * 12 + heightInches) * 0.0254;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    const resultElement = document.getElementById("result");
+    let category = "";
+    if (bmi < 18.5) {
+      category = "Under weight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      category = "Normal weight";
+    } else if (bmi >= 25 && bmi < 29.9) {
+      category = "Overweight";
+    }
+    let resultMessage = "Your BMI: " + bmi.toFixed(2) + "<br>";
+    resultMessage += "category: " + category;
+
+    resultElement.innerHTML = resultMessage;
+  }
+});
